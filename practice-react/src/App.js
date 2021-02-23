4ae5e16a7b98e3943225ea199e4a2bccf942fc61
Jorge Paul <jserranopaul@gmail.com>
2021-02-23 13:34:24 +0100

    Fetch in useEffect

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,23 +1,15 @@
-// https://reactjs.org/docs/hooks-intro.html
 import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  // Destructurin assignment https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
-  // useState allows us to have state in functional components
-  const [date, setDate] = useState(new Date());
-  // useEffect will run when the component mounts (componentDidMount) or whenenever a value in its array of dependencies [] change (componentDidUpdat), the return in the callback function will be executed when the component will unmount (componentWillUnmount)
+  const [posts, setPosts] = useState();
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {date.toLocaleString()}</h2>
-    </div>
-  );
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(json => setPosts(json))
+      .catch(e => console.log('Request failed: ', e));
+  });
+  return <ul>{posts ? posts.map(post => <li key={post.id}>{post.title}</li>) : 'Loading...'}</ul>;
 };
 
 export default App;
